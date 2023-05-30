2c8b68499bc8a9d093251153da6de0eca0278a8f
shreeshnath <134498320+shreeshnath@users.noreply.github.com>
2023-05-30 10:37:11 +0530

    Add files via upload

---
+++ os lab , program 10.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+file = open('file_aide.txt', "rt")
+data = file.read()
+words = data.split()
+
+print('Number of words in text file :', len(words))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
